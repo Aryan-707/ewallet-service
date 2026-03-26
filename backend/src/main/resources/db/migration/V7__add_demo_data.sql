dd289808b1628aecfa64528fa63e2614e09a3704
aryan-aggarwal <aryanaggarwal0420@gmail.com>
2026-03-26 16:44:33 +0530

    Fix duplicate id bug in flyway migration

--- backend/src/main/resources/db/migration/V7__add_demo_data.sql
+++ backend/src/main/resources/db/migration/V7__add_demo_data.sql
@@ -1,7 +1,13 @@
-INSERT INTO public.user (id, first_name, last_name, username, email, "password")
-VALUES (4, 'Demo', 'Recruiter', 'recruiter', 'recruiter@demo.com', '$2a$10$89RcEfrANkqDohZRV4m9ouS/SebEpZ7upOUso68BQriHRtdL7A33W');
+DO $$
+DECLARE
+  new_id BIGINT;
+BEGIN
+  -- We use nextval so we never clash with current registrations
+  SELECT nextval('user_seq') INTO new_id;
+  
+  INSERT INTO public.user (id, first_name, last_name, username, email, "password")
+  VALUES (new_id, 'Demo', 'Recruiter', 'recruiter', 'recruiter@demo.com', '$2a$10$89RcEfrANkqDohZRV4m9ouS/SebEpZ7upOUso68BQriHRtdL7A33W');
 
-INSERT INTO public.user_role (user_id, role_id) VALUES(4, 1);
-INSERT INTO public.user_role (user_id, role_id) VALUES(4, 2);
-
-SELECT setval('user_seq', max(id)) FROM public.user;
+  INSERT INTO public.user_role (user_id, role_id) VALUES(new_id, 1);
+  INSERT INTO public.user_role (user_id, role_id) VALUES(new_id, 2);
+END $$;
